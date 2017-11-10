3bc5471990fc8a18dfba5bd1d08f337e18c57852
dharmaraj <aimdharma1990@gmail.com>
2017-11-10 23:40:09 +0530

    second1

--- app.js
+++ app.js
@@ -6,13 +6,7 @@ const UserPasswordReset = 'Change Password';
 const USBEnable = 'USB Enable';
 const AccountPasswordReset='AccountPasswordReset';
 
-var https = require('https');
-var fs = require('fs');
 
-var options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
 
 
 // Setup Restify Server
