883e9e7aa94d188cb15ba2f78f322ae71c2ba6d5
dharmaraj <aimdharma1990@gmail.com>
2017-11-11 00:55:41 +0530

    test111

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const USBEnable = 'USB Enable';
 const AccountPasswordReset='AccountPasswordReset';
 
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port =  process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.IP  || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 var server = restify.createServer(); 
 server.listen(server_port, server_ip_address, function () {
